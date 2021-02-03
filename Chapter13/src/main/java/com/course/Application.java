31002466991072f26f03503765400221dd0a871f
xiaorewl <xiaore-wl@163.com>
2021-02-03 11:54:34 +0800

    修改Application.java

--- Chapter13/src/main/java/com/course/Application.java
+++ Chapter13/src/main/java/com/course/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableOpenApi
 @EnableScheduling
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
